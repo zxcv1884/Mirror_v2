ab80963683f02b80854271bc9efca83e28666c89
zxcv1884 <zxcv1884@gmail.com>
2019-01-20 22:10:21 +0800

    2019-01-20

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const moment = require('moment');
 const elNow = document.querySelector('.now-time');
+const elDay = document.querySelector('.now-day');
 timer();
 function timer() {
-    elNow.innerText = moment().format('HH:mm:ss');
+    elNow.innerText = moment().locale('zh-tw').format('HH:mm:ss');
+    elDay.innerText = moment().locale('zh-tw').format('YYYY年MM月DD日dddd');
     setTimeout(() => {
         timer()
     }, 1000)
