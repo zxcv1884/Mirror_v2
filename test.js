328a413bd278f6d4581ee5fdf0733230e3a2b232
moondram74 <39982517+moondram74@users.noreply.github.com>
2019-01-24 00:25:15 +0800

    20190124_關閉按鈕優化

---
+++ test.js
@@ -0,0 +1,5 @@
+const remote = require('electron').remote;
+document.getElementById("close-btn").addEventListener("click", function (e) {
+    var window = remote.getCurrentWindow();
+    window.close();
+});
