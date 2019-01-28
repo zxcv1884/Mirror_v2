29a680b64c0de98fdd76033992417cf39588171f
zxcv1884 <zxcv1884@gmail.com>
2019-01-28 22:53:55 +0800

    back

--- main.js
+++ main.js
@@ -16,7 +16,6 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-  mainWindow.webContents.openDevTools({ mode : "right"})
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
--- test.js
+++ test.js
@@ -1,6 +1,4 @@
 const remote = require('electron').remote;
-
-
 document.getElementById("close-btn").addEventListener("click", function (e) {
     var window = remote.getCurrentWindow();
     window.close();
