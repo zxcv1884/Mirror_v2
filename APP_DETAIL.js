e6ae48e8a89ffaa5459a4a91e8f5476e907159d5
moondram74 <39982517+moondram74@users.noreply.github.com>
2019-01-30 01:55:35 +0800

    20190130_SWIPER測試

--- APP_DETAIL.js
+++ APP_DETAIL.js
@@ -1,23 +1,35 @@
-var youtube = document.createElement("button");
+
+var youtube = document.createElement("div");
+youtube.id = "youtube";
+youtube.style.backgroundImage = "url(app_picture/YOUTUBE.png)";
+youtube.className = "app swiper-slide";
+
+var facebook = document.createElement("div");
+facebook.id = "facebook";
+facebook.style.backgroundImage = "url(app_picture/FACEBOOK.png)";
+facebook.className = "app swiper-slide";
+
+var weather = document.createElement("div");
+weather.id = "weather";
+weather.style.backgroundImage = "url(app_picture/WEATHER.png)";
+weather.className = "app swiper-slide";
 
 
-    youtube.style.borderBottomStyle = "none";
 
-    youtube.style.borderColor = "black";
-    youtube.style.backgroundImage="url(app_picture/001.png)";
-    youtube.style.backgroundColor = "black";
-    youtube.style.backgroundSize = "100% 100%";
-    youtube.style.paddingTop = "20%";
-    youtube.style.paddingBottom = "20%";
-    youtube.style.paddingRight = "32.5%";
-    youtube.style.paddingLeft = "32.5%";
-    youtube.id = "youtube";
+var j = document.getElementById("swiper-wrapper");
+j.appendChild(youtube);
+j.appendChild(facebook);
+j.appendChild(weather);
 
-
-var b = document.createElement("button");
-    b.innerHTML = "bbb";
-
-var j = document.getElementById("aj");
-    j.appendChild(youtube);
-
-
+var all = document.querySelectorAll(".app");
+var i;
+for (i = 0; i < all.length; i++) {
+    all[i].style.borderBottomStyle = "none";
+    all[i].style.borderColor = "black";
+    all[i].style.backgroundColor = "black";
+    all[i].style.backgroundSize = "100% 100%";
+    all[i].style.paddingTop = "20%";
+    all[i].style.paddingBottom = "20%";
+    all[i].style.paddingRight = "32.5%";
+    all[i].style.paddingLeft = "32.5%";
+}
