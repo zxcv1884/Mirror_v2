fa56dc9247f2a3bd44a074aabc1ab4500c5c02b1
zxcv1884 <zxcv1884@gmail.com>
2019-02-20 18:02:58 +0800

    2019-02-20

--- app.js
+++ app.js
@@ -1,11 +1,2 @@
-const moment = require('moment');
-const elNow = document.querySelector('.now-time');
-const elDay = document.querySelector('.now-day');
-timer();
-function timer() {
-    elNow.innerText = moment().locale('zh-tw').format('HH:mm:ss');
-    elDay.innerText = moment().locale('zh-tw').format('YYYY年MM月DD日dddd');
-    setTimeout(() => {
-        timer()
-    }, 1000)
-}
+require("./moment.js");
+require("./weather.js");
--- weather.js
+++ weather.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql');
+const pic = document.querySelector('.weather-pic');
+const weather = document.querySelector('.now-weather');
 const con = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -13,19 +15,28 @@ con.query("SELECT `district` FROM `personalization` WHERE `user` = 'zxcv1884@gma
         timer();
     } else {
         console.log('使用者未設定住址');
+        pic.innerText = "使用者未設定住址";
     }
 });
-// const pic = document.querySelector('.weather-pic');
-// const weather = document.querySelector('.now-weather');
-function timer() {
-        if (district !== "") {
-            con.query("SELECT * FROM `weathers` WHERE `鄉鎮` ='"+ district +"' ORDER BY `weathers`.`時間` ASC", function (error, result) {
-                console.log(result[0]);
-                let time = '2019-01-29 18:00:00';
-                var dt = new Date(time);
-                dt.setHours( dt.getHours() + 3 );
-                console.log(dt);
-            });
-        }
+const WxMap = new Map();
+    let temp = [1,2,4,6,8,15,19,21,23,24,29,34,37,42];
+for(let i of temp ){
+    let p = 1;
+    while (p<=i) {
+        WxMap.set(p,i);
+        p++;
+    }
 }
-setInterval(timer,1000*10);
+
+function timer() {
+    if (district !== "") {
+        con.query("SELECT * FROM `weathers` WHERE `鄉鎮` ='" + district + "' ORDER BY `weathers`.`時間` ASC", function (error, result) {
+            let Wx = (result[0].天氣現象編號);
+            let Wd = (result[0].天氣預報綜合描述.split('。'));
+            weather.innerText='天氣'+Wd[0]+'，'+Wd[1]+'。\n'+Wd[2]+'，'+Wd[3]+'。';
+            pic.innerHTML = "<img src='weather_icon/"+ WxMap.get(Wx)+".png' alt='1'>";
+        });
+    }
+}
+
+setInterval(timer, 1000 * 60 * 30);
